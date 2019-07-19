2a82d1fcd5178c8d3ae795fea54b8f6d8a96da84
darumacity <lt012071@gmail.com>
2019-07-20 04:56:19 +0900

    Add swagger spec.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('yamljs').load('./swagger.yaml');
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/', (req, res) => res.send('Hello'));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
