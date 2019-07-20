32f19891eb6231336e7736bd2dc6dd16152dc6ae
darumacity <lt012071@gmail.com>
2019-07-20 14:35:12 +0900

    Add Basic Auth

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('yamljs').load('./swagger.yaml');
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use(require('./auth'));
+
 app.get('/', (req, res) => res.send('Hello'));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
